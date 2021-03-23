6657ee2d58065bf333c57b05ddd09911057a24dd
Vinh Tran <vctran62@users.noreply.github.com>
2021-03-23 12:09:09 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,46 @@
+#File =variable.tf
+variable "user" {
+    type = "string"
+}
+variable "password" {
+    type = "string"
+}
+variable "host" {
+    type = "string"
+}
+variable "region" {
+    type = "string"
+}
+variable "datastore" {
+    type = "string"
+}
+variable "cluster" {
+    type = "string"
+}
+variable "network_interface" {
+    type = "string"
+}
+
+variable "domain" {
+    type = "string"
+}
+
+variable "hostname" {
+    type = "string"
+}
+
+variable "templateName" {
+    type = "string"
+}
+
+variable "vmname" {
+    type = "string"
+}
+
+variable "foldername" {
+    type = "string"
+}
+
+variable "diskpath" {
+    type = "string"
+}
